e665348e1616dc03bfffeec71212bfbdce5a74a3
Stepan Pribytko <49967730+stepanpr@users.noreply.github.com>
2021-06-25 18:33:09 +0300

    Add files via upload

---
+++ Global.hpp
@@ -0,0 +1,49 @@
+#ifndef GLOBAL_HPP
+# define GLOBAL_HPP
+# include <iostream>
+# include <sstream>
+# include <fstream>
+# include <string>
+# include <map>
+# include <vector>
+# include <iomanip>
+# include <unistd.h>
+# include <pthread.h>
+# include <algorithm>
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <unistd.h>
+# include <string.h>
+# include <sys/time.h>
+
+
+# include "Server.hpp"
+# include "Config.hpp"
+# include "RequestParser.hpp"
+# include "Exceptions.hpp"
+
+
+# define MAX_THREADS 3 /* ограничение допустимого количества потоков */
+
+# define RED  "\e[0;31m"
+# define GREEN "\033[0;32m"
+# define YELLOW "\e[0;33m"
+# define BLUE "\033[0;34m"
+# define PURPLE "\033[0;35m"
+# define CYAN "\e[0;36m"
+# define WHITE "\e[0;37m"
+# define RED_B "\033[1;31m"
+# define GREEN_B "\033[1;32m"
+# define YELLOW_B "\e[1;33m"
+# define BLUE_B "\033[1;34m"
+# define PURPLE_B "\033[1;35m"
+# define CYAN_B "\e[1;36m"
+# define WHITE_B "\e[1;37m"
+# define RESET "\033[0m"
+
+
+
+
+#endif
